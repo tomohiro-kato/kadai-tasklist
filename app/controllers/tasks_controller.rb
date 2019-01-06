42c1109e1074aa81082c63d91eae9286b678df71
tomohiro.kato <tomohiro.cells@gmail.com>
2019-01-06 14:48:28 +0000

    taskDRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,19 +1,17 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
+  
   def index
     @tasks = Task.all
   end
-
   def show
-    @task = Task.find(params[:id])
   end
-
   def new
     @task = Task.new
   end
-
   def create
     @task = Task.new(task_params)
-
     if @task.save
       flash[:success] = 'task が正常に登録されました'
       redirect_to @task
@@ -22,14 +20,9 @@ class TasksController < ApplicationController
       render :new
     end
   end
-
   def edit
-     @task = Task.find(params[:id])
   end
-
   def update
-   @task = Task.find(params[:id])
-    
     if @task.update(task_params)
       flash[:success] ='taskが正常に更新されました'
       redirect_to @task
@@ -37,24 +30,18 @@ class TasksController < ApplicationController
       flash.now[:danger] ='task が更新されませんでした'
       render :edit
     end  
-    
-    
   end
-
   def destroy
-    @task =Task.find(params[:id])
     @task.destroy
-
     flash[:success] = 'task は正常に削除されました'
     redirect_to tasks_url 
-    
-    
   end
   private
-  
+  def set_task
+    @task=Task.find(params[:id])
+  end
   #strong parameter
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content , :status)
   end
-  
 end
